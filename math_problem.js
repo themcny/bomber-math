f8117203009ce0af306c3abf46b305b6d4e997f3
Eric DellAringa <edella2@gmail.com>
2016-02-20 11:19:08 -0800

    Add socket.io add node.js, create chat feature

---
+++ math_problem.js
@@ -0,0 +1,14 @@
+
+
+function getRandomInt(min, max) {
+    return Math.floor(Math.random() * (max - min + 1)) + min;
+}
+
+function Problem() {
+  this.number1 = getRandomInt(1, 12);
+  this.number2 = getRandomInt(1, 12);
+  this.question = this.number1 + " * " + this.number2 + " = ?";
+  this.answer = this.number1 * this.number2;
+
+}
+
