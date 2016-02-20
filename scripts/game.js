b16cff844dc00adfbf85b10c84eab3daa8190de3
Nicole Yee <n.lindsey.yee@gmail.com>
2016-02-20 12:09:49 -0800

    weird things are happening

--- scripts/game.js
+++ scripts/game.js
@@ -1,15 +1,27 @@
 var canvas = document.getElementById('canvas');
 var ctx = canvas.getContext('2d');
 var posX = 100.0;
-var posY = 175.0;
+var posY = 176.0;
 var velX = 0.0;
 var velY = 0.0;
 var gravity = 0.5;
 var onGround = false;
-window.addEventListener("mousedown", startJump, false);
-window.addEventListener("mouseup", endJump, false);
+// window.onload = jump();
+// window.addEventListener("mousedown", startJump, false);
+// window.addEventListener("mouseup", endJump, false);
+
+window.addEventListener('keypress', function (e) {
+    if (e.keyCode !== 13) {
+        console.log('enter')
+        startJump();
+        endJump();
+        resetVel();
+    }
+}, false);
 loop();
 function startJump(){
+  console.log('startJump')
+  console.log(velX)
   if(onGround){
     velX = 4.0;
     velY = -12.0;
@@ -20,13 +32,15 @@ function endJump(){
   if(velY < -6.0){
     velY = -6.0;
   }
+  console.log('endJump')
 }
+function resetVel(
 function loop(){
-  Update();
-  Render();
-  window.setTimeout(loop, 33);
+  update();
+  render();
+  window.setTimeout(loop, 33 );
 }
-function Update(){
+function update(){
   velY += gravity;
   posY += velY;
   posX += velX;
@@ -38,11 +52,11 @@ function Update(){
 // if(posX < 10 || posX > 190)
 //     velX *= -1;
 }
-function Render(){
-  ctx.clearRect(0, 0, 200, 200);
+function render(){
+  ctx.clearRect(0, 0, 500, 500);
   ctx.beginPath();
-  ctx.moveTo(0,175);
-  ctx.lineTo(200,175);
+  ctx.moveTo(0,176);
+  ctx.lineTo(500,175);
   ctx.stroke();
   ctx.beginPath();
   ctx.moveTo(posX - 10, posY - 20);
