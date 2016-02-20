3f50d58c71ebd9ffc8d33e5337b6c0a40f0abbd7
Eric DellAringa <edella2@gmail.com>
2016-02-20 14:47:26 -0800

    Add players on connection. unique id

--- index.js
+++ index.js
@@ -2,20 +2,28 @@ var express = require('express');
 var app = express();
 var http = require('http').Server(app);
 var io = require('socket.io')(http);
-var connectedUsers = {};
+var lastPlayerId = 0;
+var players = {};
+
 
 app.use(express.static(__dirname + '/public'));
 
 app.get('/', function(req, res){
-  res.sendfile('index.html');
+  res.sendFile('index.html');
 });
 
 io.on('connection', function(socket){
-  connectedUsers["player_1"] = socket;
+  var newComer = new Player({id: lastPlayerId});
+  players[lastPlayerId] = newComer;
+  socket.player = newComer;
+  lastPlayerId ++;
+  console.log(newComer);
+
   socket.on('chat message', function(msg){
     io.emit('chat message', msg);
   });
 });
+
 
 http.listen(3000, function(){
   console.log('listening on http://192.168.1.75:3000');
@@ -26,6 +34,7 @@ http.listen(3000, function(){
 
 
 
-function Player() {
+function Player(options) {
   this.health = 10;
+  this.id = options.id;
 }
