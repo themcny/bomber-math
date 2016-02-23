373e7bea545245f2c6b8a0ce25b27670ee5b4ca5
Daniel Woznicki <tokay89@sbcglobal.net>
2016-02-23 15:15:29 -0800

    Update port

--- server.js
+++ server.js
@@ -11,7 +11,7 @@ app.get('/', function(req, res){
   res.sendfile('index.html');
 });
 
-http.listen(3000, function(){
+http.listen(process.env.PORT || 3000, function(){
   console.log('listening on port*:3000');
 });
 
